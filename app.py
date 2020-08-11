f64bcc08911e4c781add59182434b6572c9af383
Notdetected <37031932+Notdet3cted@users.noreply.github.com>
2020-08-11 19:02:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ def start():
                 id = dms[i]['id']
 
                 if len(message) is not 0 and len(message) < 280:
-                    if "`umkfess!" in message.lower():
-                        message = message.replace("`umkfess!", "~UMK BASE")
+                    if "ayolomba!" in message.lower():
+                        message = message.replace("ayolomba!", "Ayolomba!")
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
                                 print("DM will be posted")
